fcb0f8d167d9ce5791fac84bde2af3298fdc9447
Marsha Annon”git config --global user.name Marsha <marshaannon@gmail.com>
2021-02-12 13:02:40 -0500

    add relationship between speakers and session models

--- src/main/java/com/marshaannon/conferencedemo/models/Speaker.java
+++ src/main/java/com/marshaannon/conferencedemo/models/Speaker.java
@@ -1,9 +1,7 @@
 package com.marshaannon.conferencedemo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "speakers")
 public class Speaker {
@@ -15,6 +13,9 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
+
+    @ManyToMany(mappedBy = "speakers")
+    private List<Session> sessions;
 
     public Speaker() {
     }
@@ -65,5 +66,13 @@ public class Speaker {
 
     public void setSpeaker_bio(String speaker_bio) {
         this.speaker_bio = speaker_bio;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
     }
 }
